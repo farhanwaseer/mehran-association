d87288876c86d1bbb648152cf1f810451efbfc17
farhanwaseer <farhanaliwaseer@gmail.com>
2025-07-29 13:47:25 +0500

    ,.

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["react-router"],
+//     },
+//   },
+//   base: "./",
+// });
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router"],
-    },
-  },
-  base: "./",
-});
+  // ✅ remove rollupOptions.external
+})
