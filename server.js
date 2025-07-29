572d96cde67deedd044ed7f0eb3365324a4ac819
farhanwaseer <farhanaliwaseer@gmail.com>
2025-07-29 11:13:08 +0500

    update

--- server.js
+++ server.js
@@ -5,20 +5,26 @@ import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-
+import path from "path";
+import { fileURLToPath } from "url";
 // config dotenv
 dotenv.config();
 
 // DB config
 connectDB();
 
+// --dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // rest object
 const app = express();
 
 // middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
